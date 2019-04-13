8b0310691df094d78d693778e9924231f50c6d7f
StalenStan <stanhoenson@icloud.com>
2019-04-13 18:27:50 +0200

    upload test works, playing songs from firebase storage works :)

---
+++ app/src/main/java/com/example/ikpmd_eindopdracht/AddTrackActivity.java
@@ -0,0 +1,58 @@
+package com.example.ikpmd_eindopdracht;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+
+public class AddTrackActivity extends AppCompatActivity {
+
+    private StorageReference mStorageRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_track);
+    }
+
+    public void upload(View v) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            }
+        }
+
+        Uri file = Uri.fromFile(new File("/storage/emulated/0/Download/73igtt9khyd11.jpg"));
+        StorageReference riversRef = mStorageRef.child("images/test.jpg");
+
+        riversRef.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Task<Uri> downloadUrl = taskSnapshot.getMetadata().getReference().getDownloadUrl();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+
+                    }
+                });
+    }
+
+}
--- app/src/main/java/com/example/ikpmd_eindopdracht/MainActivity.java
+++ app/src/main/java/com/example/ikpmd_eindopdracht/MainActivity.java
@@ -1,27 +1,33 @@
 package com.example.ikpmd_eindopdracht;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import com.example.ikpmd_eindopdracht.database.RealtimeDatabaseController;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    RealtimeDatabaseController rdc = new RealtimeDatabaseController();
-    FirebaseDatabase database;
-    DatabaseReference myRef;
+    private StorageReference mStorageRef;
+
+    private RealtimeDatabaseController rdc = new RealtimeDatabaseController();
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
     }
 
     public void write(View v) {
@@ -32,5 +38,27 @@ public class MainActivity extends AppCompatActivity {
         Object object = rdc.readFromDatabase("message");
         System.out.println(object);
     }
+
+    public void openAddTrackActivity(View v) {
+        startActivity(new Intent(MainActivity.this, AddTrackActivity.class));
+    }
+
+    public void playSong(View v) {
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/ikpmd-eindopdracht.appspot.com/o/07%20Skinny%20Dip.mp3?alt=media&token=0410cd97-628d-4ed7-8cc9-8e2cad301634");
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
