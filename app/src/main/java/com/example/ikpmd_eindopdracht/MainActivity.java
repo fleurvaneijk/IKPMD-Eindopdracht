053c5e656f4ec978eb8c2c32f5dcd716e0b771d3
fleurvaneijk <s1102696@student.hsleiden.nl>
2019-04-23 21:20:22 +0200

    clean :)

--- app/src/main/java/com/example/ikpmd_eindopdracht/MainActivity.java
+++ app/src/main/java/com/example/ikpmd_eindopdracht/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.ikpmd_eindopdracht;
 
-import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -11,31 +9,21 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
-import com.example.ikpmd_eindopdracht.database.RealtimeDatabaseController;
 import com.example.ikpmd_eindopdracht.fragment.AccountFragment;
 import com.example.ikpmd_eindopdracht.fragment.PlaylistFragment;
 import com.example.ikpmd_eindopdracht.fragment.StatisticsFragment;
 import com.example.ikpmd_eindopdracht.fragment.TracksFragment;
-import com.example.ikpmd_eindopdracht.model.Track;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseDatabase database;
     private DatabaseReference myRef;
     private StorageReference mStorageRef;
-
-    private RealtimeDatabaseController rdc = new RealtimeDatabaseController();
-
     private DrawerLayout drawer;
 
     @Override
@@ -99,39 +87,4 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public void onSignOutClicked() {
         //sign out
     }
-
-    public void write(View v) {
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("tracks");
-        Map<String, Track> tracks = new HashMap<>();
-
-        myRef.setValue(tracks);
-    }
-
-    public void read(View v) {
-        Object object = rdc.readFromDatabase("message");
-    }
-
-    public void openAddTrackActivity(View v) {
-        startActivity(new Intent(MainActivity.this, AddTrackActivity.class));
-    }
-
-    public void playSong(View v) {
-        MediaPlayer mediaPlayer = new MediaPlayer();
-        try {
-            mediaPlayer.setDataSource("https://firebasestorage.googleapis.com/v0/b/ikpmd-eindopdracht.appspot.com/o/07%20Skinny%20Dip.mp3?alt=media&token=0410cd97-628d-4ed7-8cc9-8e2cad301634");
-            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    mp.start();
-                }
-            });
-
-            mediaPlayer.prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
 }
