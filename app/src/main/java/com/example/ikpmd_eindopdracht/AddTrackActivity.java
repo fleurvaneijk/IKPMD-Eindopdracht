9676c311f9d0ff1e206389591beed20f61eda40b
StalenStan <stanhoenson@icloud.com>
2019-04-24 23:03:31 +0200

    filechooser + uploaden works

--- app/src/main/java/com/example/ikpmd_eindopdracht/AddTrackActivity.java
+++ app/src/main/java/com/example/ikpmd_eindopdracht/AddTrackActivity.java
@@ -1,6 +1,7 @@
 package com.example.ikpmd_eindopdracht;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -10,6 +11,7 @@ import android.os.Environment;
 import android.os.ParcelFileDescriptor;
 import android.provider.OpenableColumns;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -17,23 +19,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.ikpmd_eindopdracht.model.Track;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
 
+import dmax.dialog.SpotsDialog;
+
 public class AddTrackActivity extends AppCompatActivity {
 
-    private StorageReference storageRef;
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private static final int PICK_IMAGE_CODE = 1000;
+    Button btn_upload;
+    ImageView image_view;
+    AlertDialog dialog;
+
+    StorageReference storageReference;
 
     private File imagePath = new File("/storage/emulated/0/Download/73igtt9khyd11.jpg");    //only works on stan's phone
     private File trackPath = new File("/storage/emulated/0/Download/Ariana Grande - Thank u, next.mp3");    //only works on stan's phone
@@ -44,6 +59,51 @@ public class AddTrackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_track);
+
+        dialog = new SpotsDialog.Builder().setContext(this).build();
+        btn_upload = (Button)findViewById(R.id.ConfirmBtn);
+
+        storageReference = FirebaseStorage.getInstance().getReference("image_upload");
+
+        btn_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select picture"), PICK_IMAGE_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_CODE) {
+            dialog.show();
+
+            UploadTask uploadTask = storageReference.putFile(data.getData());
+
+            Task<Uri> task = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if(!task.isSuccessful()) {
+                        Toast.makeText(AddTrackActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                    return storageReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if(task.isSuccessful()) {
+                        String url = task.getResult().toString().substring(0, task.getResult().toString().indexOf("&token") - 1);
+                        Log.d("DIRECTLINK", url);
+
+                        dialog.dismiss();
+                    }
+                }
+            });
+        }
     }
 
     public void pickImage(View v) {
@@ -60,56 +120,56 @@ public class AddTrackActivity extends AppCompatActivity {
 //        uploadTrack();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public void uploadImage(View v) {
-        // TODO: 18/04/19 Filemanager openen i.p.v. hardoced jpg
+//    @RequiresApi(api = Build.VERSION_CODES.O)
+//    public void uploadImage(View v, Uri fili) {
+//        // TODO: 18/04/19 Filemanager openen i.p.v. hardoced jpg
+//
+//        String filename = imagePath.toPath().getFileName().toString();
+//        Log.d("filename", "uploadImage: " + filename);
+//
+//        Uri file = Uri.fromFile(imagePath);
+//        storageRef = FirebaseStorage.getInstance().getReference("images/");
+//        StorageReference imageRef = storageRef.child(filename);
+//
+//        imageRef.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//            @Override
+//            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                track.setImageURL(taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
+//                Log.d("imgURL", "onSuccess: " + track.getImageURL().toString());
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception exception) {
+//
+//            }
+//        });
+//    }
 
-        String filename = imagePath.toPath().getFileName().toString();
-        Log.d("filename", "uploadImage: " + filename);
-
-        Uri file = Uri.fromFile(imagePath);
-        storageRef = FirebaseStorage.getInstance().getReference("images/");
-        StorageReference imageRef = storageRef.child(filename);
-
-        imageRef.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                track.setImageURL(taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
-                Log.d("imgURL", "onSuccess: " + track.getImageURL().toString());
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-
-            }
-        });
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public void uploadTrack(View v) {
-        // TODO: 18/04/19 Filemanager openen i.p.v. hardoced jpg
-
-        String filename = trackPath.toPath().getFileName().toString();
-
-        Log.d("filename", "uploadTrack: " + filename);
-
-        Uri file = Uri.fromFile(trackPath);
-        storageRef = FirebaseStorage.getInstance().getReference("tracks/");
-        StorageReference trackRef = storageRef.child(filename);
-
-        trackRef.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                track.setTrackURL(taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
-                Log.d("trackURL", "onSuccess: " + track.getTrackURL().toString());
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-
-            }
-        });
-    }
+//    @RequiresApi(api = Build.VERSION_CODES.O)
+//    public void uploadTrack(View v) {
+//        // TODO: 18/04/19 Filemanager openen i.p.v. hardoced jpg
+//
+//        String filename = trackPath.toPath().getFileName().toString();
+//
+//        Log.d("filename", "uploadTrack: " + filename);
+//
+//        Uri file = Uri.fromFile(trackPath);
+//        storageRef = FirebaseStorage.getInstance().getReference("tracks/");
+//        StorageReference trackRef = storageRef.child(filename);
+//
+//        trackRef.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//            @Override
+//            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                track.setTrackURL(taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
+//                Log.d("trackURL", "onSuccess: " + track.getTrackURL().toString());
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception exception) {
+//
+//            }
+//        });
+//    }
 
     public void getInputValues(View v) {
         String title = ((EditText)findViewById(R.id.TitleField)).getText().toString();
@@ -121,94 +181,66 @@ public class AddTrackActivity extends AppCompatActivity {
         this.track.setGenre(genre);
     }
 
-    public void addTrack(View v) {
-        // TODO: 18/04/19 add track model to database
-
-        DatabaseReference myRef = this.database.getReference(this.track.getTitle());
-        myRef.setValue(this.track);
-    }
-
-    public void filechooser(View v) {
-//        Permission is not yet granted
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-//            Permission has been previously asked and denied
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            }
-//            Permission has not been asked yet -> so will ask
-            else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-            }
-        }
-//        Permission is granted
-        else if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            Intent intent = new Intent();
-            intent.setType("image/*");
-            intent.setAction(Intent.ACTION_GET_CONTENT);
-            startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent returnIntent) {
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            Uri returnUri = returnIntent.getData();
-            Log.d("uri", "" + returnUri);
-
-            Cursor returnCursor = getContentResolver().query(returnUri, null, null, null, null);
-            Log.d("cursor", "" + returnCursor);
-
-            int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
-            returnCursor.moveToFirst();
-            Log.d("filename","" + returnCursor.getString(nameIndex));
-
-//            String path = data.getData().getPath();
-//            Log.d("path", "" + path);
+//    public void addTrack(View v) {
+//        // TODO: 18/04/19 add track model to database
 //
-//            File file = new File(data.getData().getPath());
-//            Log.d("onActivityResult", "" + file.getAbsolutePath());
-//
-//            Uri file2 = Uri.fromFile(imagePath);
-//            Log.d("uri shit", "onActivityResult: " + file2);
-//
-//            List<String> pathSegements = data.getData().getPathSegments();
-//            Log.d("pathsegements", "onActivityResult: " + pathSegements);
+//        DatabaseReference myRef = this.database.getReference(this.track.getTitle());
+//        myRef.setValue(this.track);
+//    }
 
-//            URL file3 =
-        }
-    }
+//    public void filechooser(View v) {
+////        Permission is not yet granted
+//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+////            Permission has been previously asked and denied
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+//            }
+////            Permission has not been asked yet -> so will ask
+//            else {
+//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+//            }
+//        }
+////        Permission is granted
+//        else if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+//            Intent intent = new Intent();
+//            intent.setType("image/*");
+//            intent.setAction(Intent.ACTION_GET_CONTENT);
+//            startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
+//        }
+//    }
 
-//    public void duration() {
-//        Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
-//        StorageReference riversRef = storageRef.child("images/rivers.jpg");
+//    @RequiresApi(api = Build.VERSION_CODES.O)
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent returnIntent) {
+//        if (requestCode == 1 && resultCode == RESULT_OK) {
+//            Uri returnUri = returnIntent.getData();
+//            Log.d("uri", "" + returnUri);
+//            Log.d("uri", "" + returnUri.getPath());
 //
-//        File localFile = null;
-//        try {
-//            localFile = File.createTempFile("images", "jpg");
-//            riversRef.getFile(localFile)
-//                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
-//                        @Override
-//                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-//                            // Successfully downloaded data to local file
-//                            // ...
-//                        }
-//                    }).addOnFailureListener(new OnFailureListener() {
+//            Uri file = Uri.fromFile(new File(returnUri.getPath()));
+//            Log.d("file?", "" + file);
+//
+//            StorageReference riversRef = storageRef.child(file.getLastPathSegment());
+//            UploadTask uploadTask = riversRef.putFile(file);
+//
+//            uploadTask.addOnFailureListener(new OnFailureListener() {
 //                @Override
 //                public void onFailure(@NonNull Exception exception) {
-//                    // Handle failed download
+//                    // Handle unsuccessful uploads
+//                }
+//            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                @Override
+//                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
 //                    // ...
 //                }
 //            });
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
 //
-//        MediaPlayer mediaPlayer = new MediaPlayer();
-//        try {
-//            mediaPlayer.setDataSource(trackPath.toString());
-//        } catch (IOException e) {
-//            e.printStackTrace();
+//            Cursor returnCursor = getContentResolver().query(returnUri, null, null, null, null);
+//            Log.d("cursor", "" + returnCursor);
+//
+//            int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+//            returnCursor.moveToFirst();
+//            Log.d("filename","" + returnCursor.getString(nameIndex));
 //        }
-//        int duration = mediaPlayer.getDuration();
-//        Log.d("duration:", "" + duration);
 //    }
 }
