c77713e48d696aac37b7b2da917b4bd013d3bd64
fleurvaneijk <s1102696@student.hsleiden.nl>
2019-04-24 13:18:11 +0200

    google auth import

--- app/src/main/java/com/example/ikpmd_eindopdracht/SignInActivity.java
+++ app/src/main/java/com/example/ikpmd_eindopdracht/SignInActivity.java
@@ -1,10 +1,8 @@
 package com.example.ikpmd_eindopdracht;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
@@ -13,13 +11,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -39,7 +33,7 @@ public class SignInActivity extends AppCompatActivity {
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken("420379333178-sbcv0f50v6su612p5cs50ck1ukbf9kqh.apps.googleusercontent.com")
                 .requestEmail()
                 .build();
 
@@ -69,7 +63,7 @@ public class SignInActivity extends AppCompatActivity {
         updateUI(account);
         Log.d("kaas", "boven");
         Log.d("kaas", "eentje onder");
-        Log.d("kaas", account.getEmail());
+//        Log.d("kaas", account.getEmail());
 
     }
 
