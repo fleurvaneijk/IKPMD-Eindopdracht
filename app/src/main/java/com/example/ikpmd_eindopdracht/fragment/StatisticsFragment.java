152453d7d47c6ac9922ace7f39794e461afa48d0
fleurvaneijk <s1102696@student.hsleiden.nl>
2019-04-24 23:03:38 +0200

    start of statistics

--- app/src/main/java/com/example/ikpmd_eindopdracht/fragment/StatisticsFragment.java
+++ app/src/main/java/com/example/ikpmd_eindopdracht/fragment/StatisticsFragment.java
@@ -9,26 +9,26 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.ikpmd_eindopdracht.R;
+import com.example.ikpmd_eindopdracht.model.Track;
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class StatisticsFragment extends Fragment {
 
-    private BarChart barChart;
+    private static HashMap<String, Integer> favouritedTracks = new HashMap<String, Integer>();
+    private HorizontalBarChart barChart;
     private View view;
     public static final int MAX_ECTS = 60;
     public static int currentEcts = 0;
@@ -36,55 +36,27 @@ public class StatisticsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         view =  inflater.inflate(R.layout.fragment_statistics, container, false);
-
         this.makeChart();
-
         return view;
     }
 
     private void makeChart(){
-        BarChart chart = (BarChart) view.findViewById(R.id.chart);
+        HorizontalBarChart chart = (HorizontalBarChart) view.findViewById(R.id.chart);
         this.barChart = chart;
-        Description d = new Description();
-        d.setText("Piechart die weergeeft hoeveel EC's in een jaar goed zijn.");
-        barChart.setDescription(d);
-        barChart.setTouchEnabled(false);
-        barChart.getLegend().setEnabled(true);
-        barChart.animateY(1400, Easing.EaseInOutQuad);
-
-        setData(0);
-
-//        Button plus = (Button) view.findViewById(R.id.plusTweeTest);
-//        plus.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (currentEcts < MAX_ECTS) {
-//                    setData(currentEcts += 2);
-//                } else {
-//                    setData(currentEcts = 0);
-//                }
-//            }
-//        });
-//
-//        Button min = (Button) view.findViewById(R.id.minTweeTest);
-//        min.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (currentEcts < MAX_ECTS) {
-//                    setData(currentEcts -= 2);
-//                } else {
-//                    setData(currentEcts = 0);
-//                }
-//            }
-//        });
     }
 
     private void setData(int aantal) {
-        currentEcts = aantal;
         List<BarEntry> yValues = new ArrayList<>();
         List<BarEntry> xValues = new ArrayList<>();
+
+
+        for (Map.Entry<String, Integer> entry : favouritedTracks.entrySet()) {
+            String key = entry.getKey();
+            int value = (Integer)entry.getValue();
+
+//            yValues.add(new BarEntry(key, value));
+        }
 
         yValues.add(new BarEntry(aantal, 0));
         xValues.add(new BarEntry(aantal, 60));
@@ -108,11 +80,20 @@ public class StatisticsFragment extends Fragment {
         BarDataSet dataSet = new BarDataSet(yValues, "ECTS");
         dataSet.setColors(colors);//colors);
 
-        // PieDataSet set = new PieDataSet(xValues, "Election Results");
 
         BarData data = new BarData(dataSet);
         barChart.setData(data); // bind dataset aan chart.
         barChart.invalidate();  // Aanroepen van een redraw
         Log.d("aantal =", ""+currentEcts);
+    }
+
+    public static void addToFavouritedTracks (String trackTitle) {
+        if(favouritedTracks.containsKey(trackTitle)){
+            int value = (Integer)favouritedTracks.get(trackTitle);
+            favouritedTracks.put(trackTitle, value + 1);
+        }
+        else {
+            favouritedTracks.put(trackTitle, 1);
+        }
     }
 }
