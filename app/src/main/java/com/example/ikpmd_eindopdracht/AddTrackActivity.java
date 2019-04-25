c232564b034b58038436eba2e02a697e547f71ba
fleurvaneijk <s1102696@student.hsleiden.nl>
2019-04-25 14:00:48 +0200

    addTrackActivity cleanify

--- app/src/main/java/com/example/ikpmd_eindopdracht/AddTrackActivity.java
+++ app/src/main/java/com/example/ikpmd_eindopdracht/AddTrackActivity.java
@@ -1,22 +1,16 @@
 package com.example.ikpmd_eindopdracht;
 
-import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
-import android.os.Environment;
-import android.os.ParcelFileDescriptor;
+import android.os.Bundle;
 import android.provider.OpenableColumns;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -27,7 +21,6 @@ import android.widget.Toast;
 import com.example.ikpmd_eindopdracht.model.Track;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
@@ -36,15 +29,12 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.io.File;
-
 import dmax.dialog.SpotsDialog;
 
 public class AddTrackActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE_CODE = 1000;
     Button btn_upload;
-    ImageView image_view;
     AlertDialog dialog;
     String filename;
     StorageReference imageStorageReference;
@@ -62,6 +52,32 @@ public class AddTrackActivity extends AppCompatActivity {
         this.pickImage();
         this.pickTrack();
 
+    }
+
+    public void pickImage() {
+        btn_upload = (Button)findViewById(R.id.ImagePicker);
+        btn_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select picture"), PICK_IMAGE_CODE);
+            }
+        });
+    }
+
+    public void pickTrack() {
+        btn_upload = (Button)findViewById(R.id.TrackPicker);
+        btn_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("audio/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select picture"), PICK_IMAGE_CODE);
+            }
+        });
     }
 
     @Override
@@ -146,102 +162,12 @@ public class AddTrackActivity extends AppCompatActivity {
     }
 
     public void addTrack(View v) {
-        // TODO: 18/04/19 add track model to database
         this.getInputValues();
         Log.d("TRACK", "addTrack: " + track.getTrackURL() + track.getImageURL() + track.getTitle());
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("tracks/" + track.getArtist() + " - " + track.getTitle());
         myRef.setValue(track);
+        startActivity(new Intent(this, MainActivity.class));
     }
-
-    public void pickImage() {
-        // TODO: 18/04/19 opens filechooser and remembers image
-
-        btn_upload = (Button)findViewById(R.id.ImagePicker);
-        btn_upload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select picture"), PICK_IMAGE_CODE);
-            }
-        });
-    }
-
-    public void pickTrack() {
-        // TODO: 18/04/19 opens filechooser and remembers track
-        btn_upload = (Button)findViewById(R.id.TrackPicker);
-        btn_upload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("audio/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select picture"), PICK_IMAGE_CODE);
-            }
-        });
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public void upload(View v) {
-//        uploadImage();
-//        uploadTrack();
-    }
-
-//    @RequiresApi(api = Build.VERSION_CODES.O)
-//    public void uploadImage(View v, Uri fili) {
-//        // TODO: 18/04/19 Filemanager openen i.p.v. hardoced jpg
-//
-//        String filename = imagePath.toPath().getFileName().toString();
-//        Log.d("filename", "uploadImage: " + filename);
-//
-//        Uri file = Uri.fromFile(imagePath);
-//        storageRef = FirebaseStorage.getInstance().getReference("images/");
-//        StorageReference imageRef = storageRef.child(filename);
-//
-//        imageRef.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-//            @Override
-//            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-//                track.setImageURL(taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
-//                Log.d("imgURL", "onSuccess: " + track.getImageURL().toString());
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception exception) {
-//
-//            }
-//        });
-//    }
-
-//    @RequiresApi(api = Build.VERSION_CODES.O)
-//    public void uploadTrack(View v) {
-//        // TODO: 18/04/19 Filemanager openen i.p.v. hardoced jpg
-//
-//        String filename = trackPath.toPath().getFileName().toString();
-//
-//        Log.d("filename", "uploadTrack: " + filename);
-//
-//        Uri file = Uri.fromFile(trackPath);
-//        storageRef = FirebaseStorage.getInstance().getReference("tracks/");
-//        StorageReference trackRef = storageRef.child(filename);
-//
-//        trackRef.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-//            @Override
-//            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-//                track.setTrackURL(taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
-//                Log.d("trackURL", "onSuccess: " + track.getTrackURL().toString());
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception exception) {
-//
-//            }
-//        });
-//    }
-
-
-
-
 }
